5571365ac307121b47880de8efe8b73428b315b5
Parth <parthwagh13@gmail.com>
2026-03-20 16:44:29 +0530

    Added comments to CS50P first lecture and finalized it

--- learning1_returncalculator.py
+++ learning1_returncalculator.py
@@ -1,7 +1,9 @@
+# Ask users for a number
 def main():
     x = int(input("Enter a number "))
     print ("the square of number is", square(x) )
 
+#Define a square function you can also use pow comman
 def square(n):
     return n * n
 
