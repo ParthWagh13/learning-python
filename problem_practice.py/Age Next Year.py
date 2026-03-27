eae8bae0497443acd026144520d54f0e62ab0781
Parth <parthwagh13@gmail.com>
2026-03-27 17:46:08 +0530

    Solved a basic problem without functions

---
+++ problem_practice.py/Age Next Year.py
@@ -0,0 +1,8 @@
+#Ask user for thier current age
+age = int(input("What is your age? "))
+
+#Define another variable and add 1 to thier age
+x = age + 1
+
+#Use f function
+print (f"Your age next year is", x)
