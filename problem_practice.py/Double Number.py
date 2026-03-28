2f901c78b4549e1ccb9cd6231a70804607a24695
Parth <parthwagh13@gmail.com>
2026-03-28 17:22:07 +0530

    Made a number doubler with function

---
+++ problem_practice.py/Double Number.py
@@ -0,0 +1,9 @@
+#Define a main function
+def main():
+    x = int(input("Enter a number: "))
+    print (double(x))
+#Define anoter function which returns the value twice
+def double(x):
+    return x * 2
+
+main()    
