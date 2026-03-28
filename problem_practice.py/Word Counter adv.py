ed4ed806eca3c7b28b9cf5f09bd718f1d4848da7
Parth <parthwagh13@gmail.com>
2026-03-28 17:15:08 +0530

    Added comments to word counter

--- problem_practice.py/Word Counter adv.py
+++ problem_practice.py/Word Counter adv.py
@@ -1,8 +1,9 @@
+#Defined a main function
 def main():
     text = input("Enter a sentence: ")
     print(words(text))
-
+#Created another function which returns the value
 def words(text):
-    return len(text.split())
+    return len(text.split()) #Remember that split function can only be used after a variable
 
 main()
