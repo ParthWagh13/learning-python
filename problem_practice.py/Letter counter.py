49be9f7e58ee406533044d37cd319f6fd5d9ec0e
Parth <parthwagh13@gmail.com>
2026-03-29 17:48:55 +0530

    Added comments

--- problem_practice.py/Letter counter.py
+++ problem_practice.py/Letter counter.py
@@ -1,3 +1,8 @@
+#Create a simple variable
 text = input("Enter a word: ")
+
+#Create another variable specifically for len function
 word = len(text)
+
+#Use the f string
 print(f"The number of letters are {word}")
