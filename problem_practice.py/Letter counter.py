ea265f28120d7c489177e6cd3a61a7c6e1340dcd
Parth <parthwagh13@gmail.com>
2026-03-29 17:42:32 +0530

    added comments

--- problem_practice.py/Letter counter.py
+++ problem_practice.py/Letter counter.py
@@ -1,7 +1,9 @@
+#Create a main function and use the f string
 def main():
     text = input("Enter a word: ")
     print(f"The number of letters is {letter_counter(text)}")
 
+#Defined a function which uses len function
 def letter_counter(text):
     return len(text)
 
