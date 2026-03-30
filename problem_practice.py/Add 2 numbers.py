3bc8c06c80a1b8ed57d77ef78203abdb3d925334
Parth <parthwagh13@gmail.com>
2026-03-30 20:07:50 +0530

    Added comments

--- problem_practice.py/Add 2 numbers.py
+++ problem_practice.py/Add 2 numbers.py
@@ -1,8 +1,9 @@
+#Define a main function
 def main():
     x = int(input("Enter a number: "))
     print(f"The square of the number is {square(x)}")
 
-
+#Define another function which multiplies the number to itself
 def square(x):
     return x * x
 
