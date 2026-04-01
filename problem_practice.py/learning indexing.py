51a83357853185bb5854f3cc6049733b0c7bd4f6
Parth <parthwagh13@gmail.com>
2026-04-01 17:12:41 +0530

    Added comments

--- problem_practice.py/learning indexing.py
+++ problem_practice.py/learning indexing.py
@@ -1,7 +1,9 @@
+#Defined a function
 def main():
     x = (input("Enter a word: "))
     print(first_letter(x))
 
+#The [] against any variable show the positions of thier characters
 def first_letter(x):
     return x[0]
 
