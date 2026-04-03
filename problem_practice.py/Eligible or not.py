bb1077cff7c8fd980952b0b10e7f0c5663ce894b
Parth <parthwagh13@gmail.com>
2026-04-03 18:55:56 +0530

    Added comments

--- problem_practice.py/Eligible or not.py
+++ problem_practice.py/Eligible or not.py
@@ -1,8 +1,10 @@
+#Define a main function
 def main():
     age = int(input("Enter your age: "))
-    print(Eligible_ornot(age))
+    print(Eligible_or_not(age))
 
-def Eligible_ornot(x):
+#Define another function and remember to use qutations after return function
+def Eligible_or_not(x):
     if x >= 18:
         return "Eligible"
     else:
