433853ff6ef644a15cc5c758dbf7efa3c25d062e
Parth <parthwagh13@gmail.com>
2026-04-03 23:07:11 +0530

    Made a number comparer

---
+++ problem_practice.py/Largest of 2.py
@@ -0,0 +1,14 @@
+def main():
+    x = int(input("Enter a number: "))
+    y = int(input("Enter a number: "))
+    print(Largest_of_two(x,y))
+
+def Largest_of_two(x,y):
+    if x > y:
+        return "The first number is larger"
+    elif y > x:
+        return "The second number is larger"
+    else:
+        return "Both numbers are equal"
+    
+main()    
