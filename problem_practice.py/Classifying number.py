a48ac80c89c277d454b8088cdcb6d1e036b54fab
Parth <parthwagh13@gmail.com>
2026-04-03 18:49:39 +0530

    Added comments

--- problem_practice.py/Classifying number.py
+++ problem_practice.py/Classifying number.py
@@ -1,7 +1,9 @@
+#Define a main function
 def main():
     number= int(input("Enter a number: "))
     print(positive_or_not(number))
 
+#Define another function make sure to use elif and else
 def positive_or_not(x):
     if x > 0:
         return "positive"
