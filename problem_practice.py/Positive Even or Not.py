7d30a3f5bb20f186c7595fcab45412a72311c924
Parth <parthwagh13@gmail.com>
2026-04-03 23:21:12 +0530

    Added Comments

--- problem_practice.py/Positive Even or Not.py
+++ problem_practice.py/Positive Even or Not.py
@@ -1,7 +1,9 @@
+#Defined a function
 def main():
     number = int(input("Enter a number: "))
     print(positive_even_or_not(number))
 
+#Define another function and use 'and' to join 2 conditions
 def positive_even_or_not(number):
     if number > 0 and number % 2 == 0:
         return "Positive Even Number"
