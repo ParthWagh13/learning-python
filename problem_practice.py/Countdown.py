b9db83dc82905c984f3219a67bbb7e4dc31a5d5a
Parth <parthwagh13@gmail.com>
2026-04-06 21:09:41 +0530

    Added comments

--- problem_practice.py/Countdown.py
+++ problem_practice.py/Countdown.py
@@ -1,10 +1,11 @@
 def main():
     count = int(input("Enter a number: "))
-    countdown(count)
+    #Do not use print function here
+    countdown(count) 
 
 def countdown(x):
     while x >= 1:
-        print(x)
+        print(x) #Use the print function instead of return as it just returns one final value while print keeps printing
         x = x - 1    
 
 main()    
