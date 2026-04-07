3123f08d4f28b9fe202a33420a3022aa8c5548fc
Parth <parthwagh13@gmail.com>
2026-04-07 19:21:12 +0530

    Made a program which counts down with a difference of 2

---
+++ problem_practice.py/Count by Twos.py
@@ -0,0 +1,12 @@
+def main():
+    count = 10
+    count_by_twos(count)
+
+def count_by_twos(count):
+    while count > 1:
+        print(count)
+        count = count - 2
+
+main()        
+
+        
