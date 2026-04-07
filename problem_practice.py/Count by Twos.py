b3819981daa52b707449697f29b82689918f4b50
Parth <parthwagh13@gmail.com>
2026-04-07 19:22:40 +0530

    Made a counter that counts upto 10 with a difference of 2

--- problem_practice.py/Count by Twos.py
+++ problem_practice.py/Count by Twos.py
@@ -1,11 +1,11 @@
 def main():
-    count = 10
+    count = 2
     count_by_twos(count)
 
 def count_by_twos(count):
-    while count > 1:
+    while count < 11:
         print(count)
-        count = count - 2
+        count = count + 2
 
 main()        
 
