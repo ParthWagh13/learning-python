11d96e35307977c6e0ac4f83de05ef4d64acdcb0
Parth <parthwagh13@gmail.com>
2026-04-07 18:12:57 +0530

    Made a 5 start counter using a while loop

---
+++ problem_practice.py/Print Stars.py
@@ -0,0 +1,10 @@
+def main():
+    count = 5
+    stars(count)
+
+def stars(count):
+        while count > 0:
+              print("*")
+              count = count - 1
+
+main()                          
