d394dca04832fbc96e6e05acc1dbd25f2d3e97fa
Parth <parthwagh13@gmail.com>
2026-04-07 19:30:53 +0530

    Made a program that asks for a specific word only or keeps repeating itself

---
+++ problem_practice.py/Password.py
@@ -0,0 +1,10 @@
+def main():
+    word  = input("Type Python: ")
+    password(word)
+
+def password(word):
+        while word != "python":
+              word = input("Type Python: ")
+
+main()         
+
