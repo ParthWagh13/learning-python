6dc1d5accae050f2654531e24d4f1852776f9246
Parth <parthwagh13@gmail.com>
2026-04-08 19:41:45 +0530

    Fixed the program and added comments

--- problem_practice.py/Positive Number.py
+++ problem_practice.py/Positive Number.py
@@ -1,9 +1,12 @@
+#Define a main function
 def main():
     x = int(input("Enter a Positive Number: "))
     number(x)
 
+#Define another function and remmeber whenever repeating sums are asked use input command not print
 def number(y):
     while y <= 0:
         y = int(input("Enter a Positive Number: "))
+    print("Thank You")
 
 main()              
