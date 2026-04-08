7445f59b070527cae3bdcb016d0ea0fd0095bbb5
Parth <parthwagh13@gmail.com>
2026-04-08 19:33:41 +0530

    Added comments

--- problem_practice.py/Count to 5.py
+++ problem_practice.py/Count to 5.py
@@ -1,4 +1,6 @@
+#Define a variable 
 count = 1
+#use a while loop and make sure = sign is used ot do =1 like "< 6"
 while count <= 5:
     print(count)
     count += 1
--- problem_practice.py/Password.py
+++ problem_practice.py/Password.py
@@ -1,7 +1,7 @@
 def main():
     word  = input("Type Python: ")
     password(word)
-
+#It is essential that the password be in "" and also make sure the variable name is not the same
 def password(word):
         while word != "python":
               word = input("Type Python: ")
