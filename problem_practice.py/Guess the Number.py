ca9f87d8d07e58b4b29a88d07d0fef56d04743d3
Parth <parthwagh13@gmail.com>
2026-04-10 20:46:16 +0530

    Improved the number guesser

--- problem_practice.py/Guess the Number.py
+++ problem_practice.py/Guess the Number.py
@@ -1,16 +1,14 @@
 def main():
-        x = int(input("Guess the number"))
-        higher_or_lower(x)
-        secret_number(x)
+        x = int(input("Guess the number: "))
+        while x != 6:
+               higher_or_lower(x)
+               x = int(input("Guess the number: "))  
+        print("Correct")
 
 def higher_or_lower(x):
     if x > 6:
             print("Lower")
     elif x < 6:
             print("Higher")
-
-def secret_number(x):
-       while x != 6:
-              x = int(input("Guess the number"))
 
 main()              
