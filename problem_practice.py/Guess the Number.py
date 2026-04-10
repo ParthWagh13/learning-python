2b581c93d2c6f99edfc8eb3c4ba80af38a116753
Parth <parthwagh13@gmail.com>
2026-04-10 20:49:42 +0530

    Added comments

--- problem_practice.py/Guess the Number.py
+++ problem_practice.py/Guess the Number.py
@@ -1,10 +1,12 @@
+#Define a main function
 def main():
         x = int(input("Guess the number: "))
+#Make the while function inside the main function and keep higher or lower in the loop as the hint needs to be after the guess
         while x != 6:
                higher_or_lower(x)
                x = int(input("Guess the number: "))  
         print("Correct")
-
+#Define a seperate function which will act ass assisting function for main loop
 def higher_or_lower(x):
     if x > 6:
             print("Lower")
