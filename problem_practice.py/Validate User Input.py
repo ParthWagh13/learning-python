bd97f8623a9fad86868f9c3fa9d7be326e280676
Parth <parthwagh13@gmail.com>
2026-04-13 17:47:26 +0530

    Added comments

--- problem_practice.py/Validate User Input.py
+++ problem_practice.py/Validate User Input.py
@@ -1,4 +1,6 @@
+#Create a variable x 
 x = int(input("Enter a number between 1 and 10: "))
-while 1 > x or x > 10 :
+#Use the usual while loop function fron 'Keep Asking'
+while 1 > x or x > 10 : #Use the or function
     x = int(input("Enter a number between 1 and 10: "))
 print("Valid Input! ")    
