947b6d1ba248344ecd141b1f513e2f16d3356d0a
Parth <parthwagh13@gmail.com>
2026-04-14 19:14:27 +0530

    Made an average calculator

---
+++ problem_practice.py/Average Calculator.py
@@ -0,0 +1,10 @@
+value = 0
+count = 0
+x = int(input("Enter a positive integer: "))
+while x >= 0:
+    value = value + x
+    count = count +1
+    x = int(input("Enter a positive integer: "))    
+average = value // count
+
+print(f"The average is {average}")
