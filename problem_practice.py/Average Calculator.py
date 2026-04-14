b409999152a719d176791623e0efea711c12de3d
Parth <parthwagh13@gmail.com>
2026-04-14 19:22:36 +0530

    Added comments

--- problem_practice.py/Average Calculator.py
+++ problem_practice.py/Average Calculator.py
@@ -1,9 +1,10 @@
+#Made the value and count 0
 value = 0
 count = 0
 x = int(input("Enter a positive integer: "))
 while x >= 0:
     value = value + x
-    count = count +1
+    count = count +1 #This is a loop calculator which tells how many times loop has been executed
     x = int(input("Enter a positive integer: "))    
 if count == 0:
     print("No values entered")
