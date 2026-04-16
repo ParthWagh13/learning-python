7a0146dc3730628759f540c9f85cc8039ecf0fce
Parth <parthwagh13@gmail.com>
2026-04-16 18:13:39 +0530

    Added comments and removed function

--- problem_practice.py/Multiplication Table.py
+++ problem_practice.py/Multiplication Table.py
@@ -1,10 +1,12 @@
-def main():
-    x = int(input("Enter a number"))
-    i = 1
-    while 10 >= i:
-        print(x * i)
-        i = i + 1
-main()        
+#Make a variable which asks for any number
+x = int(input("Enter a number:"))
+#Set a counter to 1
+i = 1
+#Start a while loop
+while 10 >= i:
+    print(x * i)
+    i = i + 1 
+
     
 
     
