01d9c6599fb61e70780e461de64628f2c880ef46
Parth <parthwagh13@gmail.com>
2026-04-17 20:32:27 +0530

    Added comments

--- problem_practice.py/Hello 3 times.py
+++ problem_practice.py/Hello 3 times.py
@@ -1,4 +1,6 @@
-count = 0
-while count < 3:
-    print("hello")
-    count += 1
+def main():
+    count = 0
+    while count < 3:
+        print("hello")
+        count += 1 #Adds to the count everytime and is essential part of loops
+main()        
